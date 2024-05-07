d5c12a62f150ec0b84404b5aedd54033da9a5cda
Seedlab2024 <seedlabtecnoparque@gmail.com>
2024-05-07 11:44:30 -0500

    arreglos

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -2,40 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Departamento
- *
- * @property int $id
- * @property string $name
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- *
- * @package App\Models
- * @mixin \Illuminate\Database\Eloquent\Builder
- */
 class Departamento extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+
+    use HasFactory;
+    
     protected $fillable = ['name'];
 
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'departamentos';
-
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var bool
-     */
+  
     public $timestamps = false;
 
     public function users(){
