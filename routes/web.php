a5a37bad655ac75360d616e1dbf0efec697b4916
Esteban J. Avila <estebanavila2704@gmail.com>
2024-05-09 10:36:52 -0500

    function mostrar aliados (falta revisar que funcione)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\AliadoApiController;
 
 use App\Http\Controllers\Api\AuthController;
 
@@ -14,3 +15,5 @@ Route::post('logout', [AuthController::class, 'logout']);
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/validate_email', [AuthController::class, 'validate_email'])->name('validate_email');
+
+Route::get('/aliados', [AliadoApiController::class, 'index'])->name('index');
