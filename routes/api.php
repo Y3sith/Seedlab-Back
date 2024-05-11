9c885029b6c1cc2c5f6f9634042c0a71fd53cce2
Group4First <brayanfigueroajerez@gmail.com>
2024-05-11 17:56:48 -0500

    Migraciones y Ubicacion
    Se actualizaron datos en semilla (users y emprendedor)que no estaban bien estructurados y presentaban inconsistencias, se añadio el controlador de ubicacion para departamentos y municipios

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\UbicacionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EmprendedorApiController;
 use App\Http\Controllers\Api\AliadoApiController;
-
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RutaApiController;use App\Http\Controllers\Api\SuperAdminController;
 
@@ -25,14 +25,20 @@ Route::post('logout', [AuthController::class, 'logout']);
 
 Route::get('/aliado', [AliadoApiController::class, 'index'])->name('index');
 
-
-Route::post('/validate_email_em', [AuthController::class, 'validate_email'])->name('validate_email');
-
-
-Route::post('/register_em', [AuthController::class, 'register'])->name('register');
-
 Route::apiResource('/ruta',RutaApiController::class);
-
 
 Route::apiResource('emprendedor',EmprendedorApiController::class);
 Route::apiResource('superadmin',SuperAdminController::class);
+
+//AuthController
+Route::post('/validate_email_em', [AuthController::class, 'validate_email'])->name('validate_email');
+Route::post('/register_em', [AuthController::class, 'register'])->name('register');
+
+//UbicacionController
+Route::get('/deps/all', [UbicacionController::class, 'listar_dep'])->name('listar_dep');
+Route::get('/mun', [UbicacionController::class, 'listar_munxdep'])->name('listar_munxdep');
+
+
+
+
+
