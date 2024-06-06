62b57bacefdda620bd1fdeefad9f83486a1312e3
Yesith <oscarbueno.012345@gmail.com>
2024-06-06 16:41:49 -0500

    no tenga que cololcar contrasea en editarAsesorXaliado heidy

--- app/Http/Controllers/Api/AliadoApiController.php
+++ app/Http/Controllers/Api/AliadoApiController.php
@@ -203,13 +203,13 @@ class AliadoApiController extends Controller
             return response()->json(['message' => 'No se encontró ningún aliado con este ID'], 404);
         }
 
-        $asesores = Aliado::findOrFail($id)->asesor()->select('id','nombre', 'apellido', 'celular', 'id_autentication')->get();
+        $asesores = Aliado::findOrFail($id)->asesor()->select('id', 'nombre', 'apellido', 'celular', 'id_autentication')->get();
 
         $asesoresConEstado = $asesores->map(function ($asesor) {
             $user = User::find($asesor->id_autentication);
 
             return [
-                'id'=>$asesor->id,
+                'id' => $asesor->id,
                 'nombre' => $asesor->nombre,
                 'apellido' => $asesor->apellido,
                 'celular' => $asesor->celular,
@@ -221,13 +221,14 @@ class AliadoApiController extends Controller
     }
 
 
-    public function dashboardAliado($idAliado){
+    public function dashboardAliado($idAliado)
+    {
         //CONTAR ASESORIASxALIADO SEGUN SU ESTADO (PENDIENTES O FINALIZADAS)
-        $finalizadas = Asesoria::where('id_aliado', $idAliado)->whereHas('horarios', function($query) {
+        $finalizadas = Asesoria::where('id_aliado', $idAliado)->whereHas('horarios', function ($query) {
             $query->where('estado', 'Finalizada');
         })->count();
 
-        $pendientes = Asesoria::where('id_aliado', $idAliado)->whereHas('horarios', function($query) {
+        $pendientes = Asesoria::where('id_aliado', $idAliado)->whereHas('horarios', function ($query) {
             $query->where('estado', 'Pendiente');
         })->count();
 
@@ -237,10 +238,10 @@ class AliadoApiController extends Controller
         $totalAsesorias = $finalizadas + $pendientes;
 
         // Calcular los porcentajes
-         // Calcular los porcentajes
-    $porcentajeFinalizadas = $totalAsesorias > 0 ? round(($finalizadas / $totalAsesorias) * 100, 2) . '%' : 0;
-    $porcentajePendientes = $totalAsesorias > 0 ? round(($pendientes / $totalAsesorias) * 100, 2) .'%' : 0;
-    
+        // Calcular los porcentajes
+        $porcentajeFinalizadas = $totalAsesorias > 0 ? round(($finalizadas / $totalAsesorias) * 100, 2) . '%' : 0;
+        $porcentajePendientes = $totalAsesorias > 0 ? round(($pendientes / $totalAsesorias) * 100, 2) . '%' : 0;
+
         return response()->json([
             'Asesorias Pendientes' => $pendientes,
             'Porcentaje Pendientes' => $porcentajePendientes,
@@ -251,54 +252,54 @@ class AliadoApiController extends Controller
     }
 
     public function gestionarAsesoria(Request $request)
-{
-    try {
-        if (Auth::user()->id_rol != 3) {
-        return response()->json(["error" => "No tienes permisos para realizar esta acción"], 401);
-    }
+    {
+        try {
+            if (Auth::user()->id_rol != 3) {
+                return response()->json(["error" => "No tienes permisos para realizar esta acción"], 401);
+            }
 
-    $asesoriaId = $request->input('id_asesoria');
-    $accion = $request->input('accion'); // aceptar o rechazar
+            $asesoriaId = $request->input('id_asesoria');
+            $accion = $request->input('accion'); // aceptar o rechazar
 
-    $asesoria = Asesoria::find($asesoriaId);
+            $asesoria = Asesoria::find($asesoriaId);
 
-    if (!$asesoria || $asesoria->id_aliado != Auth::user()->aliado->id) {
-        return response()->json(['message' => 'Asesoría no encontrada o no asignada a este aliado'], 404);
-    }
+            if (!$asesoria || $asesoria->id_aliado != Auth::user()->aliado->id) {
+                return response()->json(['message' => 'Asesoría no encontrada o no asignada a este aliado'], 404);
+            }
 
-    /*$horario = HorarioAsesoria::where('id_asesoria', $asesoriaId)->first();
+            /*$horario = HorarioAsesoria::where('id_asesoria', $asesoriaId)->first();
     if (!$horario) {
         return response()->json(['message' => 'No se encontró un horario para esta asesoría'], 404);
     }*/
 
-    /*if ($accion === 'aceptar') {
+            /*if ($accion === 'aceptar') {
         $horario->estado = 'aceptada';
         $mensaje = 'Asesoría aceptada correctamente';
     } 
-    */
-    elseif ($accion === 'rechazar') {
-        //$horario->estado = 'rechazada';
-        $asesoria->id_aliado = null;  // Establecer id_aliado a null
-        $asesoria->isorientador = true;
-        $asesoria->save(); // Guardar cambios en la asesoria
-        $mensaje = 'Asesoría rechazada correctamente';
-    } else {
-        return response()->json(['message' => 'Acción no válida'], 400);
-    }
+    */ elseif ($accion === 'rechazar') {
+                //$horario->estado = 'rechazada';
+                $asesoria->id_aliado = null;  // Establecer id_aliado a null
+                $asesoria->isorientador = true;
+                $asesoria->save(); // Guardar cambios en la asesoria
+                $mensaje = 'Asesoría rechazada correctamente';
+            } else {
+                return response()->json(['message' => 'Acción no válida'], 400);
+            }
 
-    //$horario->save();
+            //$horario->save();
 
-    return response()->json(['message' => $mensaje], 200);
-    } catch (Exception $e) {
-        return response()->json(['error' => 'Ocurrió un error al procesar la solicitud: ' . $e->getMessage()], 500);
+            return response()->json(['message' => $mensaje], 200);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Ocurrió un error al procesar la solicitud: ' . $e->getMessage()], 500);
+        }
     }
-    
-}
-public function editarAsesorXaliado(Request $request, $id){
+    public function editarAsesorXaliado(Request $request, $id)
+{
     try {
         if (Auth::user()->id_rol != 3) {
             return response()->json(["error" => "No tienes permisos para realizar esta acción"], 401);
         }
+
         $asesor = Asesor::find($id);
 
         if ($asesor) {
@@ -308,14 +309,17 @@ public function editarAsesorXaliado(Request $request, $id){
             $asesor->save();
 
             if ($asesor->auth) {
-                $user = $asesor-> auth;
-                $password = $request->input('password');
-                if (strlen($password)< 8) {
-                    $response = 'la contraseña debe tener al menos 8 caracteres';
-                    return response()->json(['message' => $response]);
+                $user = $asesor->auth;
+                if ($request->has('email')) {
+                    $user->email = $request->input('email');
                 }
-                $user->email = $request->input('email');
-                $user->password =  Hash::make($request->input('password'));
+                if ($request->has('password')) {
+                    $password = $request->input('password');
+                    if (strlen($password) < 8) {
+                        return response()->json(['message' => 'La contraseña debe tener al menos 8 caracteres'], 400);
+                    }
+                    $user->password = Hash::make($password);
+                }
                 $user->save();
             }
 
@@ -327,4 +331,5 @@ public function editarAsesorXaliado(Request $request, $id){
         return response()->json(['error' => 'Ocurrió un error al procesar la solicitud: ' . $e->getMessage()], 500);
     }
 }
+
 }
