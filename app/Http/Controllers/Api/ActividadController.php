fbdd1ae50d781aa8a9c7bc031704f54e0bd6614f
Y3sith <oscarbueno.012345@gmail.com>
2024-08-30 09:45:52 -0500

    ya me crea actividad con pdf

--- app/Http/Controllers/Api/ActividadController.php
+++ app/Http/Controllers/Api/ActividadController.php
@@ -36,87 +36,77 @@ class ActividadController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        try {
-            if (Auth::user()->id_rol != 1 && Auth::user()->id_rol != 3) {
-                return response()->json(["error" => "No tienes permisos para crear una actividad"], 401);
-            }
-
-            // if ($request->input('id_tipo_dato') == 2 || $request->input('id_tipo_dato') == 3) {
-            //     if (!$request->hasFile('fuente') || !$request->file('fuente')->isValid()) {
-            //         return response()->json(['message' => 'Debe seleccionar un archivo pdf o de imagen válido'], 400);
-            //     }
-            // } 
-    
-            $validatedData = $request->validate([
-                'nombre' => 'required|string|max:255',
-                'descripcion' => 'required|string|max:1000',
-                // 'fuente' => 'required',
-                'id_tipo_dato' => 'required|integer|exists:tipo_dato,id',
-                'id_asesor' => 'required|integer|exists:asesor,id',
-                'id_ruta' => 'required|integer|exists:ruta,id',
-                'id_aliado'=> 'required|integer|exists:aliado,id'
-            ]);
-    
-            // Verificar si la actividad ya existe
-            $existingActividad = Actividad::where([
-                ['nombre', $validatedData['nombre']],
-                ['descripcion', $validatedData['descripcion']],
-                // ['fuente', $validatedData['fuente']],
-                ['id_tipo_dato', $validatedData['id_tipo_dato']],
-                ['id_asesor', $validatedData['id_asesor']],
-                ['id_ruta', $validatedData['id_ruta']],
-                ['id_aliado', $validatedData['id_aliado']]
-            ])->first();
-    
-            if ($existingActividad) {
-                return response()->json(['error' => 'La actividad ya existe'], 409);
-            }
-
-            $fuente = null;
-                if ($request->hasFile('fuente')) {
-                    $file = $request->file('fuente');
-                    $fileName = time() . '_' . $file->getClientOriginalName();
-                    $mimeType = $file->getMimeType();
-
-                    if (strpos($mimeType, 'image') !== false) {
-                        $folder = 'imagenes';
-                    } elseif ($mimeType === 'application/pdf') {
-                        $folder = 'documentos';
-                    } elseif ($mimeType === 'application/pdf') {
-                        $folder = 'documentos';
-                    } else {
-                        return response()->json(['message' => 'Tipo de archivo no soportado para fuente'], 400);
-                    }
-
-                    $path = $file->storeAs("public/$folder", $fileName);
-                    $fuente = Storage::url($path);
-                } elseif ($request->input('fuente') && filter_var($request->input('fuente'), FILTER_VALIDATE_URL)) {
-                    $fuente = $request->input('fuente');
-                } elseif ($request->input('fuente')) {
-                    // Si se envió un texto en 'fuente', se guarda como texto
-                    $fuente = $request->input('fuente');
+        public function store(Request $request)
+        {
+            try {
+                if (Auth::user()->id_rol != 1 && Auth::user()->id_rol != 3) {
+                    return response()->json(["error" => "No tienes permisos para crear una actividad"], 401);
                 }
-    
-            $actividad = Actividad::create([
-                'nombre' => $validatedData['nombre'],
-                'descripcion' => $validatedData['descripcion'],
-                //'fuente' => $validatedData['fuente'],
-                'fuente' => $fuente, 
-                'id_tipo_dato' => $validatedData['id_tipo_dato'],
-                'id_asesor' => $validatedData['id_asesor'],
-                'id_ruta' => $validatedData['id_ruta'],
-                'id_aliado'=> $validatedData['id_aliado']
-            ]);
-            return response()->json(['message' => 'Actividad creada con éxito: ',$actividad], 201);
+                $validatedData = $request->validate([
+                    'nombre' => 'required|string|max:255',
+                    'descripcion' => 'required|string|max:1000',
+                    'id_tipo_dato' => 'required|integer|exists:tipo_dato,id',
+                    'id_asesor' => 'required|integer|exists:asesor,id',
+                    'id_ruta' => 'required|integer|exists:ruta,id',
+                    'id_aliado'=> 'required|integer|exists:aliado,id'
+                ]);
+        
+                // Verificar si la actividad ya existe
+                $existingActividad = Actividad::where([
+                    ['nombre', $validatedData['nombre']],
+                    ['descripcion', $validatedData['descripcion']],
+                    ['id_tipo_dato', $validatedData['id_tipo_dato']],
+                    ['id_asesor', $validatedData['id_asesor']],
+                    ['id_ruta', $validatedData['id_ruta']],
+                    ['id_aliado', $validatedData['id_aliado']]
+                ])->first();
+        
+                if ($existingActividad) {
+                    return response()->json(['error' => 'La actividad ya existe'], 409);
+                }
+
+                $fuente = null;
+                    if ($request->hasFile('fuente')) {
+                        $file = $request->file('fuente');
+                        $fileName = time() . '_' . $file->getClientOriginalName();
+                        $mimeType = $file->getMimeType();
+
+                        if (strpos($mimeType, 'image') !== false) {
+                            $folder = 'imagenes';
+                        } elseif ($mimeType === 'application/pdf') {
+                            $folder = 'documentos';
+                        } elseif ($mimeType === 'application/pdf') {
+                            $folder = 'documentos';
+                        } else {
+                            return response()->json(['message' => 'Tipo de archivo no soportado para fuente'], 400);
+                        }
+
+                        $path = $file->storeAs("public/$folder", $fileName);
+                        $fuente = Storage::url($path);
+                    } elseif ($request->input('fuente') && filter_var($request->input('fuente'), FILTER_VALIDATE_URL)) {
+                        $fuente = $request->input('fuente');
+                    } elseif ($request->input('fuente')) {
+                        // Si se envió un texto en 'fuente', se guarda como texto
+                        $fuente = $request->input('fuente');
+                    }
+        
+                $actividad = Actividad::create([
+                    'nombre' => $validatedData['nombre'],
+                    'descripcion' => $validatedData['descripcion'],
+                    'fuente' => $fuente, 
+                    'id_tipo_dato' => $validatedData['id_tipo_dato'],
+                    'id_asesor' => $validatedData['id_asesor'],
+                    'id_ruta' => $validatedData['id_ruta'],
+                    'id_aliado'=> $validatedData['id_aliado']
+                ]);
+                return response()->json(['message' => 'Actividad creada con éxito: ',$actividad], 201);
+                
+            }catch (Exception $e) {
+                return response()->json(['error' => 'Ocurrió un error al procesar la solicitud: ' . $e->getMessage()], 500);
+        }
             
-        }catch (Exception $e) {
-            return response()->json(['error' => 'Ocurrió un error al procesar la solicitud: ' . $e->getMessage()], 500);
-       }
-        
-        
-    }
+            
+        }
 
     /** 
      * Display the specified resource.
