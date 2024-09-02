79363b009e7b02668c84a2cb5a2db44f5566ad9e
Marly Rangel <rangelmarly@gmail.com>
2024-09-02 17:49:32 -0500

    reportes controller

---
+++ app/Http/Controllers/Api/ReportesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ReportesController extends Controller
+{
+    // public function exportarExcel()
+    // {
+    //     return Excel::download(new ReporteExport, 'reporte.xlsx');
+    // }
+}
