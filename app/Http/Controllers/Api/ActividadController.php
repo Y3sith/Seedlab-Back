151ba3faa3cf7f879e9f47fc5a50fae1b5eaaec8
Y3sith <145672787+Y3sith@users.noreply.github.com>
2024-09-16 21:24:35 -0500

    la actividad solo falta la validacion del contenido por leccion

--- app/Http/Controllers/Api/ActividadController.php
+++ app/Http/Controllers/Api/ActividadController.php
@@ -42,13 +42,6 @@ class ActividadController extends Controller
             if (Auth::user()->id_rol != 1 && Auth::user()->id_rol != 3 && Auth::user()->id_rol != 4) {
                 return response()->json(["error" => "No tienes permisos para crear una actividad"], 401);
             }
-                    // Verificar si algún campo requerido está vacío
-        // $requiredFields = ['nombre', 'descripcion', 'id_tipo_dato', 'id_ruta', 'id_aliado', 'fuente'];
-        // foreach ($requiredFields as $field) {
-        //     if (empty($request->input($field))) {
-        //         return response()->json(['message' => "Debes completar todos los campos requeridos de la actividad" ], 400);
-        //     }
-        // }
             $validatedData = $request->validate([
                 'nombre' => 'required|string',
                 'descripcion' => 'required|string',
