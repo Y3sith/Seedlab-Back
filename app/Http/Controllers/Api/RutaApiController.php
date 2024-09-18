e85860488bb8a8e0cf9a65f0c829209776aadc63
Heidy13 <h.andreaortiz1325@gmail.com>
2024-09-18 10:26:25 -0500

    ya lo del filtrado bien

--- app/Http/Controllers/Api/RutaApiController.php
+++ app/Http/Controllers/Api/RutaApiController.php
@@ -253,18 +253,24 @@ class RutaApiController extends Controller
         }
     }
 
-    public function actnividadxAliado($id, $id_aliado)
+    public function actnividadxAliado($id, $id_aliado, Request $request)
 {
     try {
         if (Auth::user()->id_rol != 1 && Auth::user()->id_rol != 3) {
             return response()->json(['error' => 'No tienes permisos para realizar esta acción'], 401);
         }
 
-        $ruta = Ruta::where('id', $id)->with(['actividades' => function($query) use ($id_aliado) {
-            if ($id_aliado) {
-                $query->where('id_aliado', $id_aliado);
-            }
-        }, 'actividades.aliado', 'actividades.asesor'])->get();
+        $estado = $request->input('estado', 'Activo');
+        $estadoBool = $estado === 'Activo' ? 1 : 0;
+
+        $ruta = Ruta::where('id', $id)
+            ->with(['actividades' => function ($query) use ($estadoBool, $id_aliado) {
+                $query->where('estado', $estadoBool);
+                if ($id_aliado) {
+                    $query->where('id_aliado', $id_aliado);
+                }
+            }, 'actividades.aliado', 'actividades.asesor'])
+            ->get();
 
         $ruta = $ruta->map(function ($r) {
             $r->actividades = $r->actividades->map(function ($actividad) {
@@ -284,18 +290,24 @@ class RutaApiController extends Controller
     }
 }
 
-public function actnividadxAsesor($id, $id_asesor)
+public function actnividadxAsesor($id, $id_asesor, Request $request)
 {
     try {
         if (Auth::user()->id_rol != 1 && Auth::user()->id_rol != 4) {
             return response()->json(['error' => 'No tienes permisos para realizar esta acción'], 401);
         }
 
-        $ruta = Ruta::where('id', $id)->with(['actividades' => function($query) use ($id_asesor) {
+        $estado = $request->input('estado', 'Activo');
+        $estadoBool = $estado === 'Activo' ? 1 : 0;
+
+        $ruta = Ruta::where('id', $id)
+        ->with(['actividades' => function ($query) use ($estadoBool, $id_asesor) {
+            $query->where('estado', $estadoBool);
             if ($id_asesor) {
                 $query->where('id_asesor', $id_asesor);
             }
-        }, 'actividades.aliado', 'actividades.asesor'])->get();
+        }, 'actividades.aliado', 'actividades.asesor'])
+        ->get();
 
         $ruta = $ruta->map(function ($r) {
             $r->actividades = $r->actividades->map(function ($actividad) {
