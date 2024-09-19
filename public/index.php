416f6bb9f5cfb2d002b35b1a92c104c03c4e3ed3
Mrangel793 <rangelmarly@gmail.com>
2024-09-18 21:27:35 -0500

    reversa index.php

--- public/index.php
+++ public/index.php
@@ -4,19 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-header('Access-Control-Allow-Headers: token, Content-Type');
-header('Access-Control-Max-Age: 1728000');
-
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__ . '/../bootstrap/app.php')
+(require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
