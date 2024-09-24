24fa04584fcb79eacee2df309947c92f20ba8a80
Mrangel793 <rangelmarly@gmail.com>
2024-09-24 17:38:31 -0500

    env

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], //https://ruta.adsocidm.com
+    'allowed_origins' => ['https://ruta.adsocidm.com'], //https://ruta.adsocidm.com
 
     'allowed_origins_patterns' => [],
 
